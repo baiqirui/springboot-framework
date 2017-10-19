9c1229e83ce2bf0514ec1fa93e4d51ad4e00b617
baiqirui <baiqirui@cnhnkj.com>
2017-10-19 13:58:02 +0800

    redis

---
+++ bqr-framework-autoconfigure/src/main/java/com/bqr/framework/redis/JedisProperties.java
@@ -0,0 +1,25 @@
+package com.bqr.framework.redis;
+
+import java.util.function.Supplier;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import lombok.Data;
+
+@Data
+//@Validated
+@ConfigurationProperties(prefix = "redis")
+public class JedisProperties {
+
+
+  public static final int DEFAULT_TIMEOUT = 2000;
+  public static final Supplier<JedisPoolProperties> DEFAULT_POOL = JedisPoolProperties::new;
+
+  // 连接地址 ip:port
+//  @NotNull
+  private String node;
+  // 超时时间
+  private int commandTimeout = DEFAULT_TIMEOUT;
+  // 连接池配置
+  private JedisPoolProperties pool = DEFAULT_POOL.get();
+}
