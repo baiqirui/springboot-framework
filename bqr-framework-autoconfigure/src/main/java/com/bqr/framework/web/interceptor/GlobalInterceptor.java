13f1057b0098f562daf4572978d7d1ed28ce8ac7
baiqirui <419475374@qq.com>
2017-11-13 15:51:41 +0800

    配置拦截器

--- bqr-framework-autoconfigure/src/main/java/com/bqr/framework/web/interceptor/GlobalInterceptor.java
+++ bqr-framework-autoconfigure/src/main/java/com/bqr/framework/web/interceptor/GlobalInterceptor.java
@@ -1,26 +1,26 @@
 package com.bqr.framework.web.interceptor;
 
-import java.util.Date;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.bqr.framework.mybatis.config.DynamicDataSourceHolder;
+
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * 全局拦截器，用于一些全局设置 Created by lvjj on 2016/11/17
  */
 
+@Slf4j
 public class GlobalInterceptor implements HandlerInterceptor
 {
-    
-    private Logger logger = LoggerFactory.getLogger(GlobalInterceptor.class);
-    
+
+
+
     /**
      * 在请求处理之前进行调用（Controller方法调用之前）
      *
@@ -34,12 +34,14 @@ public class GlobalInterceptor implements HandlerInterceptor
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
         throws Exception
     {
+
+
         // response.setContentType("application/json;charset=utf-8");
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods","GET,POST,PUT,DELETE,OPTIONS");
         response.setHeader("Access-Control-Max-Age", "2592000");
         response.setHeader("Access-Control-Allow-Headers",
-            "Content-Type,sessionId,appKey,timestamp,v,sign");
+            "Content-Type,appKey,timestamp,v,sign");
         
         
         // 2xx 状态码
@@ -60,10 +62,12 @@ public class GlobalInterceptor implements HandlerInterceptor
             default:
                 break;
         }
+
         return true;
     }
-    
-    
+
+
+
     /**
      * 请求处理之后进行调用，但是在视图被渲染之前（Controller方法调用之后）
      * 
@@ -80,25 +84,8 @@ public class GlobalInterceptor implements HandlerInterceptor
         ModelAndView modelAndView)
         throws Exception
     {
-        String accept = request.getHeader("Accept");
-        if (MediaType.APPLICATION_JSON_VALUE.equals(accept) || MediaType.ALL_VALUE.equals(accept))
-        {
-            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-        }
-        
-        // 2.4，add by yidi，API 执行时间
-        Long apiStart = (Long)request.getAttribute("api_start");
-        Long apiRuntime = (null != apiStart) ? new Date().getTime() - apiStart : 0;
-        // 应陈闻一要求，改成秒级，毫秒单位为小数位 by lvjj 20161109
-        response.addHeader("api_runtime", String.valueOf((apiRuntime / 1000)));
-        
-        // 2.5，add by yidi，打印 api 访问日志
-        StringBuffer requestUrl = request.getRequestURL();
-        if (null != request.getQueryString())
-        {
-            requestUrl.append("/").append(request.getQueryString());
-        }
-        logger.info("api runtime " + (apiRuntime / 1000.0) + " url=" + requestUrl.toString());
+        //清空线程中的数据源;
+        DynamicDataSourceHolder.clearDataSource();
     }
     
     /**
